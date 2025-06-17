b9aa7e91065d46823d521439b3d9fa0ef70562b6
AbdalrhmanFiras <aa3819341@gmail.com>
2025-06-17 23:40:50 +0300

    create merchant table

--- database/migrations/2025_06_17_195644_create_merchants_table.php
+++ database/migrations/2025_06_17_195644_create_merchants_table.php
@@ -13,7 +13,12 @@ return new class extends Migration {
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('country');
+            $table->string('city');
+            $table->string('address');
+            $table->enum('status', ['Active', 'InActive']);
             $table->timestamps();
         });
     }
